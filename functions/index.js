85657f475d15f4588cd9e2971d2f7543c8834e4f
Carlton Joseph <carlton.joseph@gmail.com>
2019-07-03 17:29:45 -0700

    add server function new post notification code

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const cors = require("cors")({ origin: true });
+const webpush = require("web-push");
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -18,7 +20,38 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
       .database()
       .ref("posts")
       .push({ id, title, location, image })
-      .then(() => response.status(201).json({ message: "data stored", id }))
+      .then(() => {
+        webpush.setVapidDetails(
+          "mailto:carlton.joseph@gmail.com",
+          "BNHpuT3_GM0OItLIxbt71a36JD4g6XD3kOJuD3iLpHUs4i3tlll4eSisMRc7Ezt5mx1D0qG2hN_N2GhXBunTKno",
+          "IyIuYXmvFSqXH7S6eAMC4_ZR_HbnjigQCbiZR3nR-qU"
+        );
+        return admin
+          .database()
+          .ref("subscriptions")
+          .once("value");
+      })
+      .then(subscriptions => {
+        subscriptions.forEach(sub => {
+          const pushConfig = {
+            endpoint: sub.val().endpoint,
+            keys: {
+              auth: sub.val().keys.auth,
+              p256dh: sub.val().keys.p256dh
+            }
+          };
+          webpush
+            .sendNotification(
+              pushConfig,
+              JSON.stringify({
+                title: "New Post",
+                content: "New post added!"
+              })
+            )
+            .catch(e => console.log("web push error", e));
+        });
+        return response.status(201).json({ message: "data stored", id });
+      })
       .catch(err => response.status(500).json({ error: err }));
   });
 });
