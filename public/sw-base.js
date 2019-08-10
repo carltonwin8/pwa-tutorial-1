51bfaefdfb88b8e60c9e7dc7f2032ef995e4cf42
Carlton Joseph <carlton.joseph@gmail.com>
2019-08-09 18:32:22 -0700

    fix post fetch code and add new post code to workbox

--- public/sw-base.js
+++ public/sw-base.js
@@ -39,13 +39,11 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
   "https://pwagram-6bbfe.firebaseio.com/posts.json",
   async ({ event }) => {
-    let r;
     return await fetch(event.request)
       .then(response => {
-        r = response;
         const clonedResp = response.clone();
         console.log("fetch happened", clonedResp);
-        clearAllData("posts")
+        return clearAllData("posts")
           .then(() => clonedResp.json())
           .then(json => {
             console.log("json", json);
@@ -54,7 +52,7 @@ workbox.routing.registerRoute(
             );
             return Promise.all(pArr);
           })
-          .then(() => r)
+          .then(() => response)
           .catch(err => console.error("Error! With posts indexDb", err));
       })
       .catch(err => console.log("Failed fetch for", event.request));
@@ -97,3 +95,83 @@ workbox.routing.registerRoute(
 );
 
 workbox.precaching.precacheAndRoute([]);
+
+self.addEventListener("sync", event => {
+  console.log("bg sync", event);
+  if (event.tag === "sync-new-post") {
+    console.log("syn new post");
+    event.waitUntil(
+      readAllData("sync-posts").then(data => {
+        for (let dt of data) {
+          console.log("db data", dt);
+          const postData = new FormData();
+          postData.append("id", dt.id);
+          postData.append("title", dt.title);
+          postData.append("location", dt.location);
+          postData.append("file", dt.picture, dt.id + ".png");
+          postData.append("rawLocationLat", dt.rawLocation.lat);
+          postData.append("rawLocationLng", dt.rawLocation.lng);
+          fetch(
+            "https://us-central1-pwagram-6bbfe.cloudfunctions.net/storePostData",
+            {
+              method: "POST",
+              body: postData
+            }
+          )
+            .then(resp => {
+              console.log("Send Data", resp);
+              if (resp.ok) {
+                resp.json().then(resData => {
+                  deletItemFromData("sync-posts", resData.id);
+                });
+              }
+            })
+            .catch(err => console.error("Error sending data", err));
+        }
+      })
+    );
+  }
+});
+
+self.addEventListener("notificationclick", event => {
+  const { notification, action } = event;
+  console.log("noti action", notification, action);
+  if (action == "confirm") {
+    console.log("Confirm was chosen");
+    notification.close();
+  } else {
+    console.log(action);
+    event.waitUntil(
+      clients.matchAll().then(clnts => {
+        const client = clnts.find(clnt => {
+          return (clnt.visibilityState = "visible");
+        });
+        if (client !== undefined) {
+          client.navigate(notification.data.url);
+          client.focus();
+        } else {
+          client.openWindow(notification.data.url);
+        }
+        notification.close();
+      })
+    );
+  }
+});
+
+self.addEventListener("notificationclose", event => {
+  console.log("noti closed", event);
+});
+
+self.addEventListener("push", event => {
+  console.log("push notification rec", event);
+  let data = { title: "New", content: "dummy data", openUrl: "/" };
+  if (!event.data) return;
+  data = JSON.parse(event.data.text());
+  const options = {
+    body: data.content,
+    icon: "/src/images/icons/app-icon-96x96.png",
+    badge: "/src/images/icons/app-icon-96x96.png",
+    data: { url: data.openUrl }
+  };
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
